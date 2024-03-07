17562647ce5cc8113dc46e11fb489fa816fc1488
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-07 16:32:18 +0200

    0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,28 +8,36 @@ from fabric.api import *
 from datetime import datetime
 from os.path import exists
 
+
 env.hosts = ['54.152.60.211', '34.229.71.198']  # <IP web-01>, <IP web-02>
-# env.user = "ubuntu"
-# env.key_filename = "~/.ssh/school"
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/school"
+
 
 def do_deploy(archive_path):
-    """Distributes the archive to the web servers."""
-    if not exists(archive_path):
-        return False  # Returns False if the file is not found
-
+    """ distributes the archive to
+    """
+    if exists(archive_path) is False:
+        return False  # Returns False if the file not found
     filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/{}'.format(filename.split('.')[0])
-    tmp = "/tmp/{}".format(filename)
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
 
     try:
         put(archive_path, "/tmp/")
+        # ^ Upload the archive
         run("mkdir -p {}/".format(no_tgz))
+        # decompress the archive
         run("tar -xzf {} -C {}/".format(tmp, no_tgz))
         run("rm {}".format(tmp))
         run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
         run("rm -rf {}/web_static".format(no_tgz))
+        # ^ Delete the archive
         run("rm -rf /data/web_static/current")
+        # Delete the link
         run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        # Create a new link
+        # linking to the new version
         return True
     except Exception as e:
         print(e)
